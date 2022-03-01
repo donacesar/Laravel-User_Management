5960ad82b82b94c7ccf8f41def003916151045e4
donacesar <donacesar@gmail.com>
2022-03-01 16:09:58 +0300

    Add HTML files to project

---
+++ laravel-User_Management/resources/views/users.blade.php
@@ -0,0 +1,13 @@
+{{--{{"Ны на странице Users"}}--}}
+
+@extends('layout')
+
+@section('title')
+    {{'Все Пользователи'}}
+@endsection
+
+@section('header-title')
+    Все пользователи
+@endsection
+
+
--- laravel-User_Management/routes/web.php
+++ laravel-User_Management/routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('test', function () {
     return view('welcome');
 });
+
+Route::get('/', 'UserController@index');
+
+
+// Групаируем префиксы в путях
+//         /admin/posts
+Route::prefix('admin')->group(function () {
+    Route::get('posts', function() {
+        echo 123;
+    });
+
+    // используем группу namespace-ов для котроллеров админки
+    Route::namespace('Admin')->group(function() {
+
+        // В итоге получится App\Http\Controllers\Admin\PostController
+        Route::get('post', 'PostController@index');
+    });
+});
+
+// потом установим middleware
+/*Route::middleware(['first', 'second'])->group(function() {
+   Route::get('test', function() {
+      // Роут использует first и second Middleware
+   });
+});*/
